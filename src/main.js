b05765517b5141ea3e24dd6eab168756d76ae760
robinndev <robson_dev@hotmail.com>
2022-05-31 00:12:45 -0300

    adding

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 import { createApp } from 'vue';
 // import VueMask from 'v-mask';
+import VueConfetti from 'vue-confetti';
 import BootstrapVue3 from 'bootstrap-vue-3';
 import Vuelidate from 'vuelidate';
+import VueSweetalert2 from 'vue-sweetalert2';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
-
+import 'sweetalert2/dist/sweetalert2.min.css';
 // JA ESTA COM VEE VALIDATE
 createApp(App).use(store).use(router).use(BootstrapVue3)
   .use(Vuelidate)
+  .use(VueConfetti)
+  .use(VueSweetalert2)
   // .use(VueMask)
   .mount('#app');
--- src/views/concluded/PageConcluded.vue
+++ src/views/concluded/PageConcluded.vue
@@ -1,3 +1,54 @@
 <template>
-  <div></div>
+  <div class="conclused">
+    <div class="conclused__whitebox">
+      <header class="concluded__header">
+        <h1>Cadastro concluido</h1>
+        <p>Obrigado por fazer parte da plataforma</p>
+      </header>
+      <div class="conclused__main">
+        <div class="concluded__text">
+          <p>
+            Obrigado por se inscrever em nossa plataforma, em nome de toda a equipe da fácil
+            consulta, te desejamos boas vinda! Lorem ipsum, dolor sit amet consectetur adipisicing
+            elit. Distinctio mollitia minus voluptatibus hic culpa modi, at, ut suscipit facilis
+            recusandae veritatis consequatur magnam natus amet illo? Reprehenderit aspernatur
+            architecto earum? consulta, te desejamos boas vinda! Lorem ipsum, dolor sit amet
+            consectetur adipisicing elit. Distinctio mollitia minus voluptatibus hic culpa modi, at,
+            ut suscipit facilis recusandae veritatis consequatur magnam natus amet illo?
+            Reprehenderit aspernatur architecto earum? consulta, te desejamos boas vinda! Lorem
+            ipsum, dolor sit amet consectetur adipisicing elit. Distinctio mollitia minus
+            voluptatibus hic culpa modi, at, ut suscipit facilis recusandae veritatis consequatur
+            magnam natus amet illo? Reprehenderit aspernatur architecto earum?
+          </p>
+        </div>
+        <div class="concluded__image">
+          <img src="@/assets/conclused.png" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import Swal from 'sweetalert2';
+
+export default {
+  methods: {
+    alert() {
+      Swal.fire({
+        icon: 'success',
+        title: 'Obrigado!',
+        text: 'Agradecemos por se cadastrar em nossa plataforma 😃',
+        footer: '<a href="https://facilconsulta.com.br/">Nos contate em caso de dúvida</a>',
+      });
+    },
+  },
+  mounted() {
+    this.alert();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import './style.scss';
+</style>
